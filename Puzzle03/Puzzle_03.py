55954763ff9cb5da366fea09e9139554cf4aff67
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-02 23:02:51 +0000

    Create Puzzle_03.py

---
+++ Puzzle03/Puzzle_03.py
@@ -0,0 +1,25 @@
+# https://adventofcode.com/2023/day/3
+
+test_input : list[str] = [
+]
+
+
+################################
+# Part (a)
+################################
+
+
+f = open("Puzzle03_test.txt")
+# f = open("Puzzle03_input.txt")
+
+f.close()
+
+
+################################
+# Part (b)
+################################
+
+
+# f = open("Puzzle03_test.txt")
+# # f = open("Puzzle03_input.txt")
+# f.close()
