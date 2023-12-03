507aa6c97c56415c089400ade0e6013b1077345e
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-03 21:17:21 +0000

    Create files for Puzzle04

---
+++ Puzzle04/Puzzle_04.py
@@ -0,0 +1,24 @@
+# https://adventofcode.com/2023/day/4
+
+f = open("Puzzle04_test.txt")
+# f = open("Puzzle04_input.txt")
+input = f.read().splitlines()
+f.close()
+
+def show(x):
+  for line in x:
+    print(line)
+  print("-" * len(x[0]))
+  print()
+
+
+################################
+# Part (a)
+################################
+
+
+
+################################
+# Part (b)
+################################
+
