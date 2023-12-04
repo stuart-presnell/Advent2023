917f129cfc793b11014d2ab89fcadcfb4ff1a50d
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-04 21:12:28 +0000

    Create files

---
+++ Puzzle05/Puzzle_05.py
@@ -0,0 +1,27 @@
+# https://adventofcode.com/2023/day/5
+
+import re
+
+f = open("Puzzle05_test.txt")
+# f = open("Puzzle05_input.txt")
+input = f.read().splitlines()
+f.close()
+
+def show(x):
+  for line in x:
+    print(line)
+  print("-" * len(x[0]))
+  print()
+
+
+show(input)
+
+################################
+# Part (a)
+################################
+
+
+################################
+# Part (b)
+################################
+
