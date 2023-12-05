b5a6beb53219f6948bf21fc1cb5f0350d4f3a797
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-05 16:15:01 +0000

    Create files for Puzzle06

---
+++ Puzzle06/Puzzle_06.py
@@ -0,0 +1,26 @@
+# https://adventofcode.com/2023/day/6
+
+f = open("Puzzle06_test.txt")
+# f = open("Puzzle06_input.txt")
+input = f.read().splitlines()
+f.close()
+
+def show(x):
+  for line in x:
+    print(line)
+  print("-" * len(x[0]))
+  print()
+
+################################
+
+
+
+################################
+# Part (a)
+################################
+
+
+################################
+# Part (b)
+################################
+
