374a9b7addebcfd862db8f1af8b9659c56d5dbd0
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-07 21:55:18 +0000

    Create files

---
+++ Puzzle08/Puzzle_08.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2023/day/8
+
+f = open("Puzzle08_test.txt")
+test_input = f.read().splitlines()
+f.close()
+
+f = open("Puzzle08_input.txt")
+input = f.read().splitlines()
+f.close()
+
+def show(x):
+  for line in x:
+    print(line)
+  print("-" * len(x[0]))
+  print()
+
+def parse_nums(s:str) -> list[int]:
+  '''Given a string of the form "a b c ... z", where each entry is a number, return a list[int]'''
+  op = s.split()
+  op = list(map(int,op))  # This will raise an error if any entry doesn't parse as a number
+  return op
+
+def rotate90(M):
+  '''Given a matrix `M` consisting of a list of strings (or a list of lists), 
+  rotate it 90 degrees clockwise'''
+  return [list(reversed([M[j][i] for j in range(len(M))]))
+            for i in range(len(M[-1]))]
+
+################################
+
+################################
+# Part (a)
+################################
+
+# def main_a(ip):
+#   pass
+
+# main_a(test_input)  # 
+# main_a(input)       # 
+
+
+################################
+# Part (b)
+################################
+
+# def main_b(ip):
+#   pass
+
+# main_b(test_input)  # 
+# main_b(input)       # 
