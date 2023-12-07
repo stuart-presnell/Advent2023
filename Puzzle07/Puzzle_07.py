f8fffe0c4fafa14795537a130bc562132c350077
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-07 05:12:34 +0000

    Create files

---
+++ Puzzle07/Puzzle_07.py
@@ -0,0 +1,38 @@
+# https://adventofcode.com/2023/day/7
+
+
+# f = open("Puzzle07_test.txt")
+f = open("Puzzle07_input.txt")
+input = f.read().splitlines()
+f.close()
+
+def show(x):
+  for line in x:
+    print(line)
+  print("-" * len(x[0]))
+  print()
+
+def parse_nums(s:str) -> list[int]:
+  '''Given a string of the form "a b c ... z", where each entry is a number, return a list[int]'''
+  op = s.split()
+  op = list(map(int,op))  # This will raise an error if any entry doesn't parse as a number
+  return op
+
+def rotate90(M):
+  '''Given a matrix `M` consisting of a list of strings (or a list of lists), 
+  rotate it 90 degrees clockwise'''
+  return [list(reversed([M[j][i] for j in range(len(M))]))
+            for i in range(len(M[-1]))]
+
+################################
+
+
+
+################################
+# Part (a)
+################################
+
+
+################################
+# Part (b)
+################################
