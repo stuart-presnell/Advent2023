449698508fb2875d17cc443f9ce1f66f4d217f8f
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-08 05:29:29 +0000

    Solution to part (a)

--- Puzzle08/Puzzle_08.py
+++ Puzzle08/Puzzle_08.py
@@ -1,5 +1,7 @@
 # https://adventofcode.com/2023/day/8
 
+from itertools import takewhile, dropwhile
+
 def show(x):
   for line in x:
     print(line)
@@ -36,21 +38,62 @@ f.close()
 # Part (a)
 ################################
 
+def parse_ip(ip_file):
+  route_code = list(takewhile(lambda x: x != "", ip_file))[0]
+  tree_list = list(dropwhile(lambda x: x != "", ip_file))[1:]  # Remove blank line at start
+  tree = {}
+  for line in tree_list:
+    [node, pair] = line.split(" = ")
+    [left, right] = pair[1:-1].split(", ")
+    tree[node] = [left,right]
+  return (route_code, tree)
 
-show(test_input_1)
+# print(tree)
 
-# def main_a(ip):
-#   pass
+# Starting with AAA, you need to look up the next element 
+# based on the next left/right instruction in your input.
 
-# main_a(test_input)  # 
-# main_a(input)       # 
+# Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
+
+def follow_route(route_code, tree, verbose = False):
+  current_node = 'AAA'
+  i = 0
+  n = len(route_code)
+  nodes_visited = []
+  while current_node != 'ZZZ':
+    if verbose: print(current_node, end='\n')
+    step = i%n    # loop around when we fall off the end of the string
+    # look up where to go next
+    if verbose: print(route_code[step])
+    if route_code[step] == 'L':
+      next_node = tree[current_node][0]
+    elif route_code[step] == 'R':
+      next_node = tree[current_node][1]
+    else:
+      raise ValueError
+    # record where we're going, then step there
+    nodes_visited.append(next_node)
+    current_node = next_node
+    i += 1
+  if verbose: print(current_node, end='\n\n')
+  return nodes_visited
+
+
+def main_a(ip_file, verbose = False):
+  (route_code, tree) = parse_ip(ip_file)
+  nv = follow_route(route_code, tree, verbose)
+  print(len(nv))
+
+main_a(test_input_1)  # 2
+main_a(test_input_2)  # 6
+main_a(input)         # 12083
 
 
 ################################
 # Part (b)
 ################################
 
-# def main_b(ip):
+# def main_b(ip_file):
 #   pass
 
 # main_b(test_input)  # 
