a0eefaa07fb2a0d0b09d25d71282889521ecbd53
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-08 07:12:30 +0000

    Delete unused functions

--- Puzzle08/Puzzle_08.py
+++ Puzzle08/Puzzle_08.py
@@ -8,26 +8,6 @@ from time import perf_counter
 TIMING = False
 if TIMING: start_time = perf_counter()
 
-
-
-def show(x):
-  for line in x:
-    print(line)
-  print("-" * len(x[0]))
-  print()
-
-def parse_nums(s:str) -> list[int]:
-  '''Given a string of the form "a b c ... z", where each entry is a number, return a list[int]'''
-  op = s.split()
-  op = list(map(int,op))  # This will raise an error if any entry doesn't parse as a number
-  return op
-
-def rotate90(M):
-  '''Given a matrix `M` consisting of a list of strings (or a list of lists), 
-  rotate it 90 degrees clockwise'''
-  return [list(reversed([M[j][i] for j in range(len(M))]))
-            for i in range(len(M[-1]))]
-
 ################################
 
 f = open("Puzzle08_test_1.txt")
