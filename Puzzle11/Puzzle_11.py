6640f6287bf4d48574fd3d0eca7f09c7937fd88c
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-11 05:29:54 +0000

    Define `find_galaxies`

--- Puzzle11/Puzzle_11.py
+++ Puzzle11/Puzzle_11.py
@@ -30,7 +30,7 @@ input      = parse_file_a("Puzzle11_input.txt")   # There are 455 '#'s in input
 expansion_test1 = parse_file_a("expansion_test1.txt")
 
 matrix = test_input
-show(matrix)
+# show(matrix)  # Original input matrix
 
 def duplicate_empty_rows(M):
   op = []
@@ -55,6 +55,17 @@ def expand_universe(M):
 matrix = expand_universe(matrix)
 
 TTT.timecheck("Expand")
+
+def find_galaxies(M):
+  # g = []
+  # for row in len(M):
+  #   for col in len(M[0]):
+  #     if M[row][col] == '#':
+  #       g.append((row,col))
+  return [(row,col) for row in range(len(M)) for col in range(len(M[0])) if M[row][col] == '#']
+
+show(matrix)    # Expanded universe
+find_galaxies(matrix)
 
 ################################
 # Part (a)
