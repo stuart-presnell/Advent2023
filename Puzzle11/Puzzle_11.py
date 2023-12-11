062749a6c4aa992ff9e2a56ed24e88cedc7c0ad7
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-11 05:32:56 +0000

    Add `Dijkstra`

--- Puzzle11/Puzzle_11.py
+++ Puzzle11/Puzzle_11.py
@@ -10,6 +10,9 @@ rotate90,
 Timer,
 )
 
+from PQueue import PQ
+from math import inf
+
 # from time import perf_counter
 # TIMING = False
 # if TIMING: start_time = perf_counter()
@@ -61,6 +64,84 @@ def find_galaxies(M):
 
 show(matrix)    # Expanded universe
 find_galaxies(matrix)
+
+
+def Dijkstra(matrix, START, END, criterion = lambda other,here: other <= here + 1, verbose = False):
+  ht = len(matrix)
+  wd = len(matrix[0])
+
+  # During each step, you can move exactly one square up, down, left, or right...
+  def neighbours(x,y):
+    '''Given a pair of coordinates, return a list of all NSWE neighbours within [0,wd) * [0,ht)'''
+    raw_neighbours = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+    return [(a,b) for (a,b) in raw_neighbours if (0 <= a < ht) and (0 <= b < wd)]
+
+  # ...but not every neighbour square is accessible; `criterion` tells us which are
+  def accessible_neighbours(x,y):
+    '''Given a pair of coordinates,
+    return a list of the neighbours that are accessible per the adjacency criterion
+    by default: whose height is at most one higher than [x,y]'''
+    return [(a,b) for (a,b) in neighbours(x,y) if criterion(matrix, (x,y), (a,b))]
+
+  # Assign to every node a tentative distance value, initialised to infinity
+  t_dist  = [[inf for _ in range(wd)] for _ in range(ht)]
+
+  (STARTx,STARTy) = START
+
+  # Give the starting node a distance of 0
+  t_dist[STARTx][STARTy] = 0
+
+  # Make a Priority Queue of nodes that have not yet been visited
+  # each item on the Priority Queue is a pair (t, (x,y)), 
+  # where t is the tentative distance to point (x,y)
+  # Being a Priority Queue means we can efficiently pop off the item with the smallest t
+  unvisited = PQ()
+  for x in range(ht):
+    for y in range(wd):
+        unvisited.add_item((x,y), t_dist[x][y])
+
+  def is_unvisited(pt):
+    (x,y) = pt
+    return unvisited.find_item((x,y))
+
+  def update_one_step():
+    # select the unvisited node that is marked with the smallest tentative distance T; 
+    # mark it as visited
+    (T, (x,y)) = unvisited.pop_item_with_priority()
+    # If the smallest T of any unvisited node is inf
+    # then we've visited every accessible node, 
+    # so tell the parent function to break
+    if T == inf:  
+      return True  # This is collected by the parent function as a variable `finished`
+    if verbose: print("Selected point (" + str(x) + "," + str(y) +") which has T = " + str(T)) 
+    unvis_neighbours = [(a,b) for (a,b) in accessible_neighbours(x,y) if is_unvisited((a,b))]
+    if verbose: print(unvis_neighbours)
+    for (a,b) in unvis_neighbours:
+      # for each unvisited neighbour, 
+      # reset its tentative distance to t+1 if that's less than its current value
+      if T+1 < t_dist[a][b]: 
+        if verbose: print("Resetting (" + str(a) + "," + str(b) +") to " + str(T+1))
+        t_dist[a][b] = T+1
+        # replace the point (a,b) in `unvisited` with new tentative distance T+1
+        unvisited.add_item((a,b), T+1)
+    return False # We don't think we've reached every reachable square yet
+
+  FINISHED = False
+  if END:
+    # If we have a destination END in mind:
+    # Keep taking steps until the destination node END is marked as visited
+    # or `update_one_step` reports that it has FINISHED exploring reachable squares
+    while (not FINISHED) & is_unvisited(END):
+      FINISHED = update_one_step()
+    return(t_dist)
+  else:  
+    # If we've passed `END = None` then walk to every square we can reach
+    while (not FINISHED) & (not unvisited.is_empty()):
+      FINISHED = update_one_step()
+    return(t_dist)
+
+
+
 
 ################################
 # Part (a)
