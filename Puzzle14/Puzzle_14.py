f8ec49e7de01140738a1fadb8643ccc250f14a42
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-14 07:09:47 +0000

    Trying to use the loop to shortcut the calculation

--- Puzzle14/Puzzle_14.py
+++ Puzzle14/Puzzle_14.py
@@ -84,10 +84,10 @@ def cycle(ip):
 # Obviously we're not going to run this code for 1 billion cycles
 # Evidently there's a loop that we need to detect
 
-def repeat_cycles(grid, max_cycles, verbose = False):
+def repeat_cycles(grid, n, verbose = False):
   '''Given a starting grid and a number of cycles to perform,
   run the cycle that many times.'''
-  for i in range(max_cycles):
+  for i in range(n):
     if verbose: print("After " + str(i+1) + " cycle" + ("s" if i > 0 else "") + ":")
     grid = cycle(grid)
     if verbose: show(grid, False)  
@@ -106,18 +106,38 @@ def detect_loop(grid, max_cycles, verbose = False):
     if grid in visited_states:
       prev = visited_states.index(grid)
       if verbose: print("Found a loop!")
-      return (grid, i, prev)
+      return (visited_states, i+1, prev)
     else:
       visited_states.append(grid)
   if verbose: print("Didn't find a loop after " + str(max_cycles) + " cycle" + plural_s(max_cycles - 1))
   return grid
 
-ip= detect_loop(ip, 11, verbose = True)
-# show(ip)
+(vs, i, prev) = detect_loop(ip, 11, verbose = False)
+
+def quick_repeat_cycles(vs, n, loop):
+  return vs[(n % loop) + 1]
+
+ip6 = repeat_cycles(ip, 6)
+ip1000 = repeat_cycles(ip, 1000)
+
+# for x in [ip6, ip1000]:
+#   print(x)
+
+billion = 1000000000
+print(billion%7)
+
+# print(vs[6+1])
+# quick_repeat_cycles(vs,100,7) == 
+
+print(repeat_cycles(ip,100))
+
+
+calc_cost(repeat_cycles(ip,billion%7))
 
 
 
 
+# show(vs)
 
 
 # def main_b(ip):
