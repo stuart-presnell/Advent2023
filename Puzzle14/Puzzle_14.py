4104265b289374979dd749b2e5f4e70cd634c384
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-14 20:45:41 +0000

    Some bumbling around from this morning

--- Puzzle14/Puzzle_14.py
+++ Puzzle14/Puzzle_14.py
@@ -23,6 +23,7 @@ input      = parse_file_a("Puzzle14_input.txt")
 
 ip = test_input
 # ip = input
+# print("Original input: ")
 # show(ip)
 
 ################################
@@ -44,7 +45,7 @@ def calc_weight_cost(row):
   op = 0
   for i in range(len(row)):
     if row[i] == 'O':
-      op += i +1
+      op += i + 1
   return op
 
 def calc_cost(grid):
@@ -84,6 +85,8 @@ def cycle(ip):
 # Obviously we're not going to run this code for 1 billion cycles
 # Evidently there's a loop that we need to detect
 
+billion = 1000000000
+
 def repeat_cycles(grid, n, verbose = False):
   '''Given a starting grid and a number of cycles to perform,
   run the cycle that many times.'''
@@ -97,54 +100,125 @@ def repeat_cycles(grid, n, verbose = False):
 def detect_loop(grid, max_cycles, verbose = False):
   '''Given a starting grid and a limit on the number of cycles to perform,
   keep running the cycle until we find a loop.'''
-  visited_states = [grid]
+  visited_states = [grid]  # After no cycles...
   for i in range(max_cycles):
     if verbose: print("After " + str(i+1) + " cycle" + ("s" if i > 0 else "") + ":")
-    grid = cycle(grid)
+    grid = cycle(grid)  # Perform one more cycle
     if verbose: show(grid, False)  
     if verbose: print()
-    if grid in visited_states:
+    if grid in visited_states:  # If the grid after i cycles is the same as some previous grid
       prev = visited_states.index(grid)
-      if verbose: print("Found a loop!")
-      return (visited_states, i+1, prev)
+      period = (i+1) - prev
+      if verbose: print("Found a loop! The grid after " + str(i+1) + " cycles is the same as after " + str(prev) + " cycles")
+      return (visited_states, period, prev)
     else:
       visited_states.append(grid)
   if verbose: print("Didn't find a loop after " + str(max_cycles) + " cycle" + plural_s(max_cycles - 1))
   return grid
 
-(vs, i, prev) = detect_loop(ip, 11, verbose = False)
+# (vs, period, loop_start) = detect_loop(ip.copy(), 1000, verbose = False)
+# print(period)
+# print(loop_start)
 
-def quick_repeat_cycles(vs, n, loop):
-  return vs[(n % loop) + 1]
+# ip2 = test_input.copy()
 
-ip6 = repeat_cycles(ip, 6)
-ip1000 = repeat_cycles(ip, 1000)
+# vs = [ip2]  # After no cycles...
+# for i in range(1000):
+#   ip2 = cycle(ip2)
+#   vs.append(ip2)
 
-# for x in [ip6, ip1000]:
-#   print(x)
-
-billion = 1000000000
-print(billion%7)
-
-# print(vs[6+1])
-# quick_repeat_cycles(vs,100,7) == 
-
-print(repeat_cycles(ip,100))
+# print("Grid after defining vs: ")
+# show(ip)
 
 
-calc_cost(repeat_cycles(ip,billion%7))
-
-
+# show(vs[:loop_start])
+# print(loop_start, loop_start + 1 * period)
+# show(vs[loop_start:loop_start + 1 * period])
+# print(loop_start + 1 * period, loop_start + 2 * period)
+# show(vs[loop_start + 1 * period:loop_start + 2 * period])
+# print(loop_start + 2 * period, loop_start + 3 * period)
+# show(vs[loop_start + 2 * period:loop_start + 3 * period])
 
 
 # show(vs)
 
+# print(vs[3])
+# k = 3
+# print(repeat_cycles(ip, k) == repeat_cycles(ip, k+6))
+# print(repeat_cycles(ip.copy(), 3))
+# print(repeat_cycles(ip.copy(), 9))
+# print(vs[9])
+# print(repeat_cycles(ip, 15))
+# for i in range(10):
+#   print(repeat_cycles(ip, 3 + 6*i))
 
-# def main_b(ip):
-#   pass
+# 01234567834567834...
 
-# print(main_b(test_input))  # 
-# print(main_b(input))       # 
+
+# print(vs[9])
+
+# print(loop_start, period)
+
+def reindex(n, loop_start, period):
+  return ((n - loop_start) % period) + loop_start
+
+def quick_repeat_cycles(vs, n, loop_start, period):
+  return vs[reindex(n, loop_start, period)]
+
+
+# print(repeat_cycles(ip,9))
+# after_billion = quick_repeat_cycles(vs, 1000000000, loop_start, period)
+# print(calc_cost(after_billion))
+
+# ip6 = repeat_cycles(ip, 6)
+# ip1000 = repeat_cycles(ip, 1000)
+
+# # for x in [ip6, ip1000]:
+# #   print(x)
+
+
+# print(billion%7)
+
+# # print(vs[6+1])
+# # quick_repeat_cycles(vs,100,7) == 
+
+# print(repeat_cycles(ip,100))
+
+
+# calc_cost(repeat_cycles(ip,billion%7))
+
+
+# print(billion%7)
+
+# show(vs)
+
+# G = test_input.copy()
+# for i in range(200):
+
+def calc_cost_b(grid):
+  '''`calc_cost` was worked out on the assumption that the grid was rotated 90.
+  That's not the case here because we've completed a full cycle, 
+  so we need to rotate the grid before calculating.'''
+  return calc_cost(rotate90(grid))
+
+
+
+def main_b(ip_file, N):
+  (vs, period, loop_start) = detect_loop(ip_file.copy(), 1000, verbose = False)
+  print("Period = ", period)
+  reduced_N = N%period if N%period > loop_start else N%period + period
+  cycled_grid = repeat_cycles(ip_file.copy(), reduced_N)
+  # k = loop_start + 8
+  # print(repeat_cycles(ip_file.copy(), k) == repeat_cycles(ip_file.copy(), k+ 30 * period))
+  # cycled_grid = vs[reindex(N, loop_start, period)]
+  return (calc_cost_b(cycled_grid))
+  
+
+print(main_b(test_input, billion))  # 64
+print(main_b(input, billion))       # 
+# 98876 is too low
+# 102130 is too high
+
 
 
 ################################
