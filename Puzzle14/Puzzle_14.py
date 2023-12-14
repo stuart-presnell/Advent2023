8c19f543062c6ef145e579da8c0c6db7f87eae0b
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-14 06:06:08 +0000

    Combine into `main_a`

--- Puzzle14/Puzzle_14.py
+++ Puzzle14/Puzzle_14.py
@@ -30,10 +30,6 @@ ip = input
 
 # Roll the rocks north
 
-# First, rotate 90 so we're working on rows instead of columns
-ip = rotate90(ip)  
-# Now our task is to roll the rocks *east*
-
 def roll_row_east(row):
   row = "".join(row)  # Make sure `row` is a string, not a list
   rolled_row = ""
@@ -43,11 +39,6 @@ def roll_row_east(row):
   # print(row)
   return (rolled_row[:len(row)])  # If we've added an excess '#', trim it off
 
-# Roll whole grid east
-rolled_grid = [roll_row_east(row) for row in ip]
-
-# print(rolled_grid[0])
-
 def calc_weight_cost(row):
   op = 0
   for i in range(len(row)):
@@ -55,28 +46,19 @@ def calc_weight_cost(row):
       op += i +1
   return op
 
-total_cost = [calc_weight_cost(row) for row in rolled_grid]
-print(sum(total_cost))
+def calc_cost(grid):
+  return sum([calc_weight_cost(row) for row in grid])
 
+def main_a(ip):
+  # First, rotate 90 so we're working on rows instead of columns
+  ip = rotate90(ip)  
+  # Now our task is to roll the rocks *east*
+  # Roll whole grid east
+  rolled_grid = [roll_row_east(row) for row in ip]
+  return calc_cost(rolled_grid)
 
-# print()
-# show(rolled_grid)
-
-  
-# for _ in range(3):
-#   rolled_grid = rotate90(rolled_grid)
-
-# rolled_grid = ["".join(row) for row in rolled_grid]
-
-# show(rolled_grid)
-# show(ip)
-
-# def main_a(ip):
-#   pass
-
-# print(main_a(test_input))  # 
-# print(main_a(input))       # 
-
+print(main_a(test_input))  # 136
+print(main_a(input))       # 112773
 
 ################################
 # Part (b)
