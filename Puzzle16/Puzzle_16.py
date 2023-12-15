d1089d603cfc6b4a5c8bf633f1b66d23a963f1e4
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-15 06:25:54 +0000

    Create files

---
+++ Puzzle16/Puzzle_16.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2023/day/00
+
+# My utility functions
+from utils import (
+show, 
+# chunk_splitlines, printT, showM, parse_nums, rotate90, close_bracket, cmp, qsort, Best, 
+# Timer,
+)
+# TTT = Timer()
+
+################################
+
+def parse_file_a(filename):
+  f = open(filename)
+  ip_file = f.read().splitlines()
+  f.close()
+  return ip_file
+
+test_input = parse_file_a("Puzzle00_test.txt")
+input      = parse_file_a("Puzzle00_input.txt")
+
+ip = test_input
+ip = input
+show(ip)
+
+################################
+# Part (a)
+################################
+
+# def main_a(ip):
+#   pass
+
+# print(main_a(test_input))  # 
+# print(main_a(input))       # 
+
+# TTT.timecheck("Part (a)")  #
+
+################################
+# Part (b)
+################################
+
+# def main_b(ip):
+#   pass
+
+# print(main_b(test_input))  # 
+# print(main_b(input))       # 
+
+# TTT.timecheck("Part (b)")  #
+
+################################
