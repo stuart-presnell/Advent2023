dd64562025eaf72c30940325a9ebf3c4d00385d0
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-16 05:33:01 +0000

    Define `one_step`; `energised` is a set

--- Puzzle16/Puzzle_16.py
+++ Puzzle16/Puzzle_16.py
@@ -21,9 +21,12 @@ def parse_file_a(filename):
 test_input = parse_file_a("Puzzle16_test.txt")
 input      = parse_file_a("Puzzle16_input.txt")
 
-ip = test_input
-# ip = input
-show(ip)
+grid = test_input
+# grid = input
+# show(grid)
+
+ht = len(grid)
+wd = len(grid[0])
 
 ################################
 # Part (a)
@@ -37,17 +40,33 @@ dir_lookup = {
   'E':(0, 1)
 }
 
-# A list of points (r,c) that have a beam passing through
-energised = [(0,0)]
+def one_step(pt, dir):
+  step = dir_lookup[dir]
+  return (pt[0] + step[0], pt[1] + step[1])
+
+
+# The set of points (r,c) that have a beam passing through
+energised = set((0,0))
 # The wavefront is an ordered dictionary of points; 
 # `wavefront[pt]` is a list of directions in which beams are moving through `pt`
 # It's ordered so at each step we can `pop` an element from it to advance
 wavefront = OrderedDict()
 wavefront[(0,0)] = ['E']
 
-while wavefront:
-  (pt, dirs) = wavefront.popitem()
+def advance_wave(pt, dir):
+  '''Given a point and a direction, advance the beam by one step in that direction, 
+  reflecting or splitting as required; 
+  put the new point(s) & dir(s) on `wavefront`, and add the points to `energised`.'''
+  global energised, wavefront
+
+  # new_pt
   pass
+
+# while wavefront:
+#   (pt, dirs) = wavefront.popitem()
+#   for dir in dirs:
+#     advance_wave(pt, dir)
+# show(energised)
 
 # def main_a(ip):
 #   pass
