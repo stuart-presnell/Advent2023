2e6b5bab0069494afa6aa7f759d29e3a74f14091
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-17 06:09:05 +0000

    Define `one_step`, start, destination

--- Puzzle17/Puzzle_17.py
+++ Puzzle17/Puzzle_17.py
@@ -27,6 +27,37 @@ show(ip)
 # Part (a)
 ################################
 
+# How to step in each cardinal direction (change in row, change in column)
+dir_lookup = {
+  'N':(-1,0),
+  'S':( 1,0),
+  'W':(0,-1),
+  'E':(0, 1)
+}
+
+ht = len(ip)
+wd = len(ip[0])
+
+def one_step(pt, dir):
+  step = dir_lookup[dir]
+  nr = pt[0] + step[0]
+  nc = pt[1] + step[1]
+  if (0 <= nr < ht) & (0 <= nc < wd):
+    return (nr, nc)
+  else:
+    # print(str((nr,nc)) + " is off the grid, so skip this!")
+    return None
+
+# new_pt = one_step(pt, dir)
+
+# Starting point is top left corner
+O = (0,0)
+# Destination is bottom right corner
+D = (ht-1, wd-1)
+
+
+
+
 # def main_a(ip_file):
 #   pass
 
