b5b95bccd44b2d11291a80c2e197c6056f1d4c47
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-18 05:39:32 +0000

    Print dug grid

--- Puzzle18/Puzzle_18.py
+++ Puzzle18/Puzzle_18.py
@@ -87,7 +87,10 @@ def get_dimensions(L):
 
 (min_r, min_c, max_r, max_c) = get_dimensions(dug)
 
-
+for row in range(min_r, max_r+1):
+  for col in range(min_c, max_c+1):
+    print('#' if (row,col) in dug else '.', end='')
+  print()
 
 
 # def main_a(ip_file):
