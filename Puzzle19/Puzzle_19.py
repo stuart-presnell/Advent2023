2ed89955622aacfe8e6632ca5131b1dfa8dc986f
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-19 05:11:49 +0000

    Create files; inputs; define `parse_machine_part`

--- Puzzle19/Puzzle_19.py
+++ Puzzle19/Puzzle_19.py
@@ -3,7 +3,8 @@
 # My utility functions
 from utils import (
 show, 
-# chunk_splitlines, printT, showM, showD, parse_nums, rotate90, 
+chunk_splitlines, 
+printT, showM, showD, parse_nums, rotate90, 
 # close_bracket, cmp, qsort, nwise_cycled,
 # Best, 
 # Timer,
@@ -14,7 +15,8 @@ show,
 
 def parse_file_a(filename):
   f = open(filename)
-  ip_file = f.read().splitlines()
+  ip_file = chunk_splitlines(f.read())
+  # .splitlines()
   f.close()
   return ip_file
 
@@ -23,7 +25,17 @@ input      = parse_file_a("Puzzle19_input.txt")
 
 ip = test_input
 # ip = input
-show(ip)
+# show(ip[1])
+
+def parse_machine_part(s):
+  '''Given a string such as `'{x=787,m=2655,a=1222,s=2876}'`, return a dictionary.'''
+  d = {}
+  for item in s[1:-1].split(","):
+    [l,n] = item.split('=')
+    d[l] = int(n)
+  return d
+
+# parse_machine_part('{x=787,m=2655,a=1222,s=2876}')
 
 ################################
 # Part (a)
