a59a9b443479a7f5b3cbaf54b83f6c16885fea41
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-21 15:14:03 +0000

    Comment out `make_wraparound_matrix`; TODO

--- Puzzle21/Puzzle_21.py
+++ Puzzle21/Puzzle_21.py
@@ -152,23 +152,29 @@ def make_matrix(grid):
   data_points = make_matrix_data(grid)
   return make_coo(data_points, ht*wd, ht*wd)
 
-def make_wraparound_matrix(grid):
-  '''Given a `grid` (represented as a list of strings), 
-  use `make_matrix_data` to assemble the adjacency data;
-  then append more adjacencies corresponding to the toroidal wrap-around across the edges;
-  then make the `coo_array`.
-  If `grid` is `ht x wd` then the number of squares is 
-  `(ht * wd)` and so the adjacency matrix is `(ht * wd) x (ht * wd)`.'''
-  ht = len(grid)
-  wd = len(grid[0])
-  data_points = make_matrix_data(grid)
-  for r in range(ht):
-    data_points.append([True, encode(r,0, wd), encode(r,wd-1, wd)])
-    data_points.append([True, encode(r,wd-1, wd), encode(r,0, wd)])
-  for c in range(wd):
-    data_points.append([True, encode(0,c, wd), encode(ht-1,c, wd)])
-    data_points.append([True, encode(ht-1,c, wd), encode(0,c, wd)])
-  return make_coo(data_points, ht*wd, ht*wd)
+
+# NO! This is not the way to generalise to part (b) --- 
+# this produces one array with toroidal boundary conditions, 
+# which would fail to count different copies of the original grid as different;
+# e.g. walking a full loop back to a copy of 'S' would count the same as revisiting 'S'.
+# TODO: Work out a better (i.e. correct!) way to account for wraparound.
+# def make_wraparound_matrix(grid):
+#   '''Given a `grid` (represented as a list of strings), 
+#   use `make_matrix_data` to assemble the adjacency data;
+#   then append more adjacencies corresponding to the toroidal wrap-around across the edges;
+#   then make the `coo_array`.
+#   If `grid` is `ht x wd` then the number of squares is 
+#   `(ht * wd)` and so the adjacency matrix is `(ht * wd) x (ht * wd)`.'''
+#   ht = len(grid)
+#   wd = len(grid[0])
+#   data_points = make_matrix_data(grid)
+#   for r in range(ht):
+#     data_points.append([True, encode(r,0, wd), encode(r,wd-1, wd)])
+#     data_points.append([True, encode(r,wd-1, wd), encode(r,0, wd)])
+#   for c in range(wd):
+#     data_points.append([True, encode(0,c, wd), encode(ht-1,c, wd)])
+#     data_points.append([True, encode(ht-1,c, wd), encode(0,c, wd)])
+#   return make_coo(data_points, ht*wd, ht*wd)
 
 
 def sparse_power(M, n):
