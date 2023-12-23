c0e9da86607783dce13ece9413c0fd1da03cf1e7
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-23 22:23:06 +0000

    Create files

---
+++ Puzzle24/Puzzle_24.py
@@ -0,0 +1,53 @@
+# https://adventofcode.com/2023/day/24
+
+# My utility functions
+from utils import (
+show, 
+# chunk_splitlines, printT, showM, showD, unzip, parse_nums, rotate90, close_bracket, cmp, qsort, nwise_cycled,
+# Best, 
+Timer,
+)
+# TTT = Timer()
+
+################################
+
+def parse_file(filename):
+  f = open(filename)
+  ip_file = f.read().splitlines()
+  f.close()
+  return ip_file
+
+test_input = parse_file("Puzzle24_test.txt")
+input      = parse_file("Puzzle24_input.txt")
+
+ip = test_input
+# ip = input
+show(ip)
+
+################################
+# Part (a)
+################################
+
+def main_a(ip_filename):
+  ip = parse_file(ip_filename)
+  pass
+
+# print(main_a("Puzzle24_test.txt"))  # 
+# print(main_a("Puzzle24_input.txt")) # 
+
+# TTT.timecheck("Part (a)")  #
+
+################################
+# Part (b)
+################################
+
+# def main_b(ip_filename):
+#   ip = parse_file(ip_filename)
+#   pass
+
+# print(main_b("Puzzle24_test.txt"))  # 
+# print(main_b("Puzzle24_input.txt")) # 
+
+# TTT.timecheck("Part (b)")  #
+
+################################
