db26263f7aff0c1d8081d0016548e98f8c05f652
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-24 06:32:29 +0000

    Define `xy_hom_coord` and `crossing`

--- Puzzle24/Puzzle_24.py
+++ Puzzle24/Puzzle_24.py
@@ -30,7 +30,7 @@ input      = parse_file("Puzzle24_input.txt")
 
 ip = test_input
 # ip = input
-show(ip)
+# show(ip)
 
 def animate_one_ns(pos,vel):
   '''Given the `pos`ition and `vel`ocity of a hailstone, 
@@ -53,6 +53,41 @@ def animate_one_ns(pos,vel):
 #   the Symmetric Equation for the line is (x-x0)/vx = (y-y0)/vy
 # TODO: SO given two such lines, they cross at some (x,y) iff this is a solution to both SEs.
 
+def xy_hom_coord(pos,vel):
+  '''Given a position `(x0,y0,z0)` and a velocity `(vx, vy, vz)`, consider just the `(x,y)` parts.
+  The `(x,y)`-trajectory of the hailstone follows a line determined by
+  - ax + by + c = 0, where
+  - a = vy
+  - b = -vx
+  - c = y0vx - x0vy
+
+  Return this triple `(a,b,c)`.'''
+  (x0,y0,_) = pos
+  (vx,vy,_) = vel
+  return (vx, -vy, y0*vx - x0*vy)
+
+# for stone in ip:
+#   print(stone, "\t", xy_hom_coord(*stone))
+
+hip = [xy_hom_coord(*stone) for stone in ip]
+
+def crossing(st1, st2):
+  '''Given the trajectories of two stones as homogeneous coordinates, 
+  return the crossing point if it exists, or `None` if they do not cross.
+  From: https://en.wikipedia.org/wiki/Line–line_intersection#Using_homogeneous_coordinates'''
+  (a1,b1,c1) = st1
+  (a2,b2,c2) = st2
+  cp = a1*b2 - a2*b1
+  if cp == 0:
+    return None
+  else:
+    ap = b1*c2 - b2*c1 
+    bp = a2*c1 - a1*c2
+    return (-bp/cp, -ap/cp, cp)
+
+for i in range(len(hip)):
+  for j in range(i+1, len(hip)):
+    print(crossing(hip[i], hip[j]))
 
 ################################
 # Part (a)
