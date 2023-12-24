848a2ab2a2938325e18b4234921ffdc543d914e6
Stuart Presnell <stuart.presnell@gmail.com>
2023-12-24 08:40:33 +0000

    Define `cross_or_not_v2` and `main_a`

--- Puzzle24/Puzzle_24.py
+++ Puzzle24/Puzzle_24.py
@@ -3,7 +3,9 @@
 # My utility functions
 from utils import (
 show, 
-# chunk_splitlines, printT, showM, showD, unzip, parse_nums, rotate90, close_bracket, cmp, qsort, nwise_cycled,
+# chunk_splitlines, printT, showM, showD, 
+unzip, 
+# parse_nums, rotate90, close_bracket, cmp, qsort, nwise_cycled,
 # Best, 
 Timer,
 )
@@ -95,11 +97,6 @@ def cross_time(p,v,x):
 # Part (a)
 ################################
 
-def main_a(ip_filename):
-  ip = parse_file(ip_filename)
-  pass
-
-
 def cross_or_not(ip, coord_min, coord_max, verbose=False):
   count = 0
   hip = [xy_hom_coord(*stone) for stone in ip]
@@ -123,13 +120,50 @@ def cross_or_not(ip, coord_min, coord_max, verbose=False):
         if verbose: print("Cross outside zone")
   return count
 
-(cross_or_not(test_input, 7, 27))  # 2
+def cross_or_not_v2(pvh1, pvh2, coord_min, coord_max, verbose=False):
+  (p1,v1,h1) = pvh1
+  (p2,v2,h2) = pvh2
 
-coord_min = 200000000000000
-coord_max = 400000000000000
-(cross_or_not(input, coord_min, coord_max))  # 27349 is too high
+  C = crossing(h1, h2)
+  if not C:
+    if verbose: print("Do not cross")
+    return False
+  (x,y,cp) = C
+  t1 = cross_time(p1, v1, x)
+  t2 = cross_time(p2, v2, x)
+  print("Crossing times: ", t1, t2)
+  # According to the penultimate example, where t=0.5, apparently t<1 is the past  (???)
+  if (t1 < 0) | (t2 < 0):  
+    if verbose: print("Crossed in the past")
+    return False
+  if (coord_min <= x <= coord_max) & (coord_min <= y <= coord_max):
+    if verbose: 
+      print(x,y)
+    return True
+  else:
+    if verbose: print("Cross outside zone")
+    return False
+
+def main_a(ip_filename, coord_min, coord_max, verbose=False):
+  ip = parse_file(ip_filename)
+  # Unfold the data into a list of positions, velocities, and hom coords
+  (P,V) = unzip(ip)
+  H = [xy_hom_coord(*stone) for stone in ip]
+  PVH = list(zip(P, V, H))
+  for i in range(len(PVH)):
+    for j in range(i+1, len(PVH)):
+      b = cross_or_not_v2(PVH[i], PVH[j], coord_min, coord_max, verbose) 
+      print(i,j,b)
+      print()
+
+# (cross_or_not(test_input, 7, 27))  # 2
+
+# coord_min = 200000000000000
+# coord_max = 400000000000000
+# (cross_or_not(input, coord_min, coord_max))  # 27349 is too high
 
 
+print(main_a("Puzzle24_test.txt", 7, 27, True)) # 
 # print(main_a("Puzzle24_input.txt")) # 
 
 # TTT.timecheck("Part (a)")  #
